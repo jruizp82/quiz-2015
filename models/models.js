618e9b5e4e798dcad650245352867b1d48cec63a
Juan Antonio Ruiz <ruiz.ja.1982@gmail.com>
2015-07-29 09:15:13 +0200

    Lista de preguntas

--- models/models.js
+++ models/models.js
@@ -26,11 +26,6 @@ var sequelize = new Sequelize(DB_name, user, pwd,
     }
 );
 
-// // Usar BBDD SQLite:
-// var sequelize = new Sequelize(null, null, null,
-//                       {dialect: "sqlite", storage: "quiz.sqlite"}
-//                     );
-
 // Importar la definición de la tabla Quiz en quiz.js
 var quiz_path = path.join(__dirname, 'quiz');
 var Quiz = sequelize.import(quiz_path);
@@ -38,14 +33,17 @@ var Quiz = sequelize.import(quiz_path);
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize.sync().success(function() {
-  // success(..) ejecuta el manejador una vez creada la tabla
-  Quiz.count().success(function(count) {
+sequelize.sync().then(function() {
+  // then(..) ejecuta el manejador una vez creada la tabla
+  Quiz.count().then(function(count) {
     if(count === 0) {   // la tabla se inicializa solo si está vacía
       Quiz.create({ pregunta:   'Capital de Italia',
                     respuesta:  'Roma'
+                  });
+      Quiz.create({ pregunta:   'Capital de Portugal',
+                    respuesta:  'Lisboa'
                   })
-      .success(function(){console.log('Base de datos inicializada')});
+      .then(function(){console.log('Base de datos inicializada')});
     };
   });
 });
--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,15 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
-});
+   res.render('index', { title: 'Quiz' });
+ });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+// Definición de rutas de /quizes
+router.get('/quizes',                                 quizController.index);
+router.get('/quizes/:quizId(\\d+)',                   quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',            quizController.answer);
 
+// GET author
 router.get('/author', function(req, res) {
   res.render('author', { title: 'Quiz' });
 });
