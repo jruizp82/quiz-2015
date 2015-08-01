a5c698faf0d963d889b4a2653459bc0597c5da93
Juan Antonio Ruiz <ruiz.ja.1982@gmail.com>
2015-08-01 22:10:52 +0200

    Crear preguntas

--- routes/index.js
+++ routes/index.js
@@ -9,12 +9,14 @@ router.get('/', function(req, res) {
  });
 
 // Autoload de comandos con :quizId
-router.param('quizId', quizController.load);    // autoload :quizId 
+router.param('quizId', quizController.load);    // autoload :quizId
 
 // Definición de rutas de /quizes
 router.get('/quizes',                                 quizController.index);
 router.get('/quizes/:quizId(\\d+)',                   quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',            quizController.answer);
+router.get('/quizes/new',                             quizController.new);
+router.post('/quizes/create',                          quizController.create);
 
 // GET author
 router.get('/author', function(req, res) {
