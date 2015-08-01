5baaff5f4878b3b7ae427f1072b2e538419d8922
Juan Antonio Ruiz <ruiz.ja.1982@gmail.com>
2015-08-01 23:42:22 +0200

    Validacion de entradas

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-   res.render('index', { title: 'Quiz' });
+   res.render('index', { title: 'Quiz', errors: [] });
  });
 
 // Autoload de comandos con :quizId
@@ -16,11 +16,11 @@ router.get('/quizes',                                 quizController.index);
 router.get('/quizes/:quizId(\\d+)',                   quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',            quizController.answer);
 router.get('/quizes/new',                             quizController.new);
-router.post('/quizes/create',                          quizController.create);
+router.post('/quizes/create',                         quizController.create);
 
 // GET author
 router.get('/author', function(req, res) {
-  res.render('author', { title: 'Quiz' });
+  res.render('author', { title: 'Quiz', errors: [] });
 });
 
 module.exports = router;
